c4f187b63fcfbe7a85d4dab2533053ff9e9c752c
chiemy <luobaolong@gmail.com>
2016-02-21 14:17:30 +0800

    goods

---
+++ app/src/main/java/com/chiemy/app/cashier/bean/Goods.java
@@ -0,0 +1,30 @@
+package com.chiemy.app.cashier.bean;
+
+import cn.bmob.v3.BmobObject;
+
+/**
+ * 商品
+ */
+public class Goods extends BmobObject {
+    public String user_id;
+    /**
+     * 条码
+     */
+    public String barcode;
+    /**
+     * 名称
+     */
+    public String name;
+    /**
+     * 供应商
+     */
+    public String supplier;
+    /**
+     * 商品进价
+     */
+    public float purchase_price;
+    /**
+     * 商品售价
+     */
+    public float selling_price;
+}
